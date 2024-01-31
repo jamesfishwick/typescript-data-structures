a02c4b763b25021c62e1d855b54765022d2950d3
Your Name <james.fishwick@willowtreeapps.com>
2024-01-31 11:47:45 -0500

    Added comments for Frequency Map

--- FrequencyMap.ts
+++ FrequencyMap.ts
@@ -1,4 +1,12 @@
+/**
+ * FrequencyMap extends the built-in Map to keep track of frequency counts for values.
+ * It overrides the set() and delete() methods to enforce using increment() and decrement() instead.
+ */
 export class FrequencyMap<T> extends Map<T, number> {
+  /**
+   * Constructor allows passing in an iterable to initialize the map.
+   * Calls increment() for each element.
+   */
   constructor(iterable?: Iterable<T>) {
     super();
     if (iterable) {
@@ -11,20 +19,37 @@ export class FrequencyMap<T> extends Map<T, number> {
   static ERROR =
     "This class uses increment and decrement methods. If you really want to use the parent method (why tho?), use Map.prototype.set() or Map.prototype.delete() instead.";
 
+  /**
+   * Overrides Map set() to throw an error.
+   * @override
+   */
   override set(key: T, value: number): never {
     throw new Error(FrequencyMap.ERROR);
   }
 
+  /**
+   * Overrides Map delete() to throw an error.
+   * @override
+   */
   override delete(key: T): never {
     throw new Error(FrequencyMap.ERROR);
   }
 
+  /**
+   * Increments the value for the given element by 1.
+   * Initializes to 0 if not present.
+   */
   increment(element: T): this {
     const count = this.get(element) || 0;
     super.set(element, count + 1);
     return this;
   }
 
+  /**
+   * Decrements the value for the given element by 1.
+   * Deletes the key if decremented to 0.
+   * Returns true if deleted, false otherwise.
+   */
   decrement(value: T): boolean {
     const count = this.get(value);
     if (count === 1) {
@@ -35,6 +60,10 @@ export class FrequencyMap<T> extends Map<T, number> {
     return false;
   }
 
+  /**
+   * Returns an array of the keys sorted by frequency count.
+   * @param ascending - Sort ascending if true, descending if false.
+   */
   sorted(ascending = true): ReadonlyArray<T> {
     const entries = [...this.entries()];
     entries.sort((a, b) => {
